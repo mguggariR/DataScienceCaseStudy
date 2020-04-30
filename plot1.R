5387a815e9cdf205df4a7544c8452d7dad1902ee
Mallik Guggari <mguggari@gmail.com>
2020-04-29 23:08:23 -0500

    cosmetic changes to plotting including title text

--- plot1.R
+++ plot1.R
@@ -1,7 +1,6 @@
 ## Read data sources in to R Dataframes
         epaRawDF <- readRDS("summarySCC_PM25.rds")
-##        sourceClassCode <- readRDS("Source_Classification_Code.rds") 
-        
+
 ## Calcualte mean emissions from all sources for each of the 1999, 2002, 2005, and 2008 years      
         y1999MeanEmissions <- mean(epaRawDF[epaRawDF$year=="1999", "Emissions"], na.rm = TRUE)
         y2002MeanEmissions <- mean(epaRawDF[epaRawDF$year=="2002", "Emissions"], na.rm = TRUE)
@@ -15,5 +14,7 @@
 ## create the plot file for plotting the results.         
         png(filename="plot1.png", width = 480, height = 480, units = "px")
         plot(yearlyEmission$years, yearlyEmission$emissions, 
-             type="l", pch=20, main="US emissions from all sources", xlab="Years", ylab="Mean Emissions in tons")
+             type="l", pch=20, 
+             main="US emissions from all sources over 1999-2008", 
+             xlab="Years", ylab="Mean Emissions in tons")
         dev.off()
